b0b145a9be152c233e47ac1435a535943d4664b6
Steffan Thomas <95288821+steffaldo@users.noreply.github.com>
2026-02-02 21:05:29 +0100

    parser

--- utils/api_parser.py
+++ utils/api_parser.py
@@ -272,3 +272,20 @@ def submit_new_surveys(df):
         api_result = call_cft_api(row)
         results.append(api_result)
     return results
+
+
+# ---- UTILITIES ---- #
+def flatten_json(obj, parent_key='', sep='.'):
+    """Recursively flattens nested dicts and lists into dot-separated keys."""
+    items = []
+    if isinstance(obj, dict):
+        for k, v in obj.items():
+            new_key = f"{parent_key}{sep}{k}" if parent_key else k
+            items.extend(flatten_json(v, new_key, sep=sep).items())
+    elif isinstance(obj, list):
+        for i, v in enumerate(obj):
+            new_key = f"{parent_key}{sep}{i}" if parent_key else str(i)
+            items.extend(flatten_json(v, new_key, sep=sep).items())
+    else:
+        items.append((parent_key, obj))
+    return dict(items)
