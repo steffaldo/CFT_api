6e32f0b9e7b464e1216656558a0b5efed7217fb3
steffaldo <steffanrl.thomas@gmail.com>
2026-02-25 15:27:53 +0100

    fixded non None

--- utils/api_parser.py
+++ utils/api_parser.py
@@ -129,7 +129,7 @@ def build_fertilizers_input(row):
     for fertilizer in FERTILIZERS:
         base_fertilizer = {
             "type": fertilizer["cft_id"], 
-            "production": fertilizer["region"] if fertilizer.get("region") else None, 
+            "production": fertilizer["region"] if fertilizer.get("region") else "", 
             "application_rate": {
                 "value": row[f"fertilizers.{fertilizer['key']}.t_per_ha"],
                 "unit": UNITS["fertilizer_application_rate"]
