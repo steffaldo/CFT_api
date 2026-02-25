922d0d51413bddd5130c866de025ad60f045c2e0
steffaldo <steffanrl.thomas@gmail.com>
2026-02-25 15:16:21 +0100

    fertilizer regions

--- utils/api_parser.py
+++ utils/api_parser.py
@@ -129,7 +129,7 @@ def build_fertilizers_input(row):
     for fertilizer in FERTILIZERS:
         base_fertilizer = {
             "type": fertilizer["cft_id"], 
-            "production": 8,  # 8 = Europe 2014
+            "production": fertilizer["region"] if fertilizer.get("region") else None, 
             "application_rate": {
                 "value": row[f"fertilizers.{fertilizer['key']}.t_per_ha"],
                 "unit": UNITS["fertilizer_application_rate"]
